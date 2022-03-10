0f06713452f72847f46255d2203d7c967db2742a
EvanHong99 <939778128@qq.com>
2022-03-10 13:45:15 +0800

    init

---
+++ myconfig.py
@@ -0,0 +1,9 @@
+# -*- coding=utf-8 -*-
+# @File     : myconfig.py
+# @Time     : 2022/3/9 20:31
+# @Author   : EvanHong
+# @Email    : 939778128@qq.com
+# @Project  : pairs_trading
+# @Description:
+
+THRESHOLD=0.95
